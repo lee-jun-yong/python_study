059243d1e7b3d24fef220c06e9e48238f85b917c
Lee Jun Yong <dothe7847@gmail.com>
2024-08-29 14:33:37 +0900

    initial commit

---
+++ bio.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // 표준 입출력 스트림을 포함합니다.
+
+// main 함수: 프로그램의 진입점
+int main() {
+    std::cout << "Hello, World!" << std::endl; // 화면에 "Hello, World!"를 출력합니다.
+    return 0; // 프로그램이 성공적으로 실행됨을 나타내는 0을 반환합니다.
+}
---
+++ python_for_j/hello.py
@@ -0,0 +1,7 @@
+
+print("hello")
+
+nums = [1,2,3,4,5,6,7,8,9,10]
+
+for i in nums:
+    print(i)
